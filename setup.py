94d9a2d836b9f96c61fdc1e83e0741365dc5be46
Roylamer <wchan757@gmail.com>
2019-02-18 23:46:52 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'nbparameterise',
+  packages = ['nbparameterise'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'Execute notebook with no timeout',
+  author = 'Icarus So',
+  author_email = 'icarus.so@gmail.com',
+  url = 'https://github.com/IcarusSO/nbparameterise', # use the URL to the github repo
+  keywords = [], # arbitrary keywords
+  classifiers = [],
+  install_requires=[
+  ]
+)
